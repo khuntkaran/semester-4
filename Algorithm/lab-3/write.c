f541edc7b8d603575259e5d9299cc954477c37de
khuntkaran <94033442+khuntkaran@users.noreply.github.com>
2022-12-22 13:40:50 +0530

    Add files via upload

---
+++ Algorithm/lab-3/write.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    FILE *fptr1,*fptr2,*fptr3;
+    long int i,number;
+
+    fptr1 = fopen("best.txt","w");
+    fptr2 = fopen("avg.txt","w");
+    fptr3 = fopen("worst.txt","w");
+
+    printf("Enter the number : ");
+    scanf("%ld",&number);
+    for(i=1;i<=number;i++){
+        fprintf(fptr1,"%d\n",i);    //best case
+        fprintf(fptr2,"%d\n",rand());    //average case
+        fprintf(fptr3,"%d\n",number-i+1);    //worst case
+    }
+    fclose(fptr1);
+    fclose(fptr2);
+    fclose(fptr3);
+}
