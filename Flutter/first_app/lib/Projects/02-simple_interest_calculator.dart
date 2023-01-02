9c3ab733f0d260664ede5c6850d172b966f12292
khuntkaran <21010101108@darshan.ac.in>
2023-01-02 23:08:50 +0530

    .

--- Flutter/first_app/lib/Projects/02-simple_interest_calculator.dart
+++ Flutter/first_app/lib/Projects/02-simple_interest_calculator.dart
@@ -6,15 +6,38 @@ class SimpleInterestCalculator extends StatefulWidget{
 }
 
 class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
+
   Widget textPrint(text){
     return Container(child: Text(text,style: TextStyle(fontSize: 20),),margin: EdgeInsets.fromLTRB(5, 20, 20, 2),);
   }
-  late TextEditingController _amount,_percentage,_time ;
 
-@override
+  Widget dataGet(key,control){
+    return Container(child: Row(
+      children: [
+        Expanded(child: textPrint(key)),
+        Expanded(child: TextField(controller: control,keyboardType: TextInputType.number,)),
+      ],
+    ),);
+  }
+
+  Widget dataPrint(key, value){
+    return Container(child: Row(
+      children: [
+        Expanded(child: textPrint(key)),
+        Expanded(child: textPrint(value,)),
+      ],
+    ),);
+  }
+
+  late TextEditingController _amount,_percentage,_time ;
+  double interest=0.0;
+  @override
   void initState() {
     super.initState();
-    _amount=_percentage=_time  = TextEditingController();
+    _amount= TextEditingController();
+    _percentage= TextEditingController();
+    _time= TextEditingController();
+    interest=100.0;
   }
 
   @override
@@ -23,51 +46,46 @@ class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
    return SafeArea(child: Scaffold(
      appBar: AppBar(title: Text("Simple Interest Calculator"),backgroundColor: Colors.pink,),
      body:  SingleChildScrollView(
-   child:Column(
-     children: [
-       Expanded(flex:2,child: Row(
+       child: Column(
          children: [
-           Expanded(child: Column(
-             children: [
-               textPrint('Enter Amount :'),
-               textPrint('Enter Amount :'),
-               textPrint('Enter Amount :'),
-             ],
-           )),
-           Expanded(child: Column(
-             children: [
-               TextField(controller: _amount,),
-               TextField(controller: _percentage,),
-               TextField(controller: _time,),
-             ],
-           )),
+           Container(
+               child: Column(
+                  children: [
+                    dataGet("Enter Amount :", _amount),
+                    dataGet("Enter Percentage :", _percentage),
+                    dataGet("Enter Time :", _time),
+                 ],
+               ),
+             margin: EdgeInsets.fromLTRB(5, 20, 5, 20),
+             ),
+           Container(
+             child: Column(
+               children: [
+                 dataPrint("Your Amount :", _amount.text),
+                 dataPrint("Your Percentage :", _percentage.text),
+                 dataPrint("Your Time :", _time.text)
+               ],
+             ),
+             margin: EdgeInsets.fromLTRB(5, 20, 5, 20),
+           ),
+           Container(
+             child: TextButton(
+               onPressed: (){setState((){});},
+               child: Text('Calculate',style: TextStyle(color: Colors.white),),
+             ),
+             color: Colors.pink,
+             margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+           ),
+           Container(
+             child: Column(
+               children: [
+                 dataPrint("Your Interest :", interest.toString()),
+               ],
+             ),
+             margin: EdgeInsets.fromLTRB(5, 20, 5, 20),
+           ),
          ],
-       )),
-       Expanded(flex:2,child: Row(
-         children: [
-           Expanded(child: Column(
-             children: [
-               textPrint('Enter Amount :'),
-               textPrint('Enter Amount :'),
-               textPrint('Enter Amount :'),
-             ],
-           )),
-           Expanded(child: Column(
-             children: [
-               textPrint(_amount.text),
-               textPrint(_percentage.text),
-               textPrint(_time.text),
-             ],
-           )),
-         ],
-       )),
-       Column(
-         children: [
-           TextButton(onPressed: (){setState(() {});}, child: Text("Calculate"))
-         ],
-       )
-     ],
-   ),
-    )));
+       ),
+     )));
   }
 }
